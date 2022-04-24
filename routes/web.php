a8416eed0996488b328822015cf029591d70fea5
austin-sanga <austinsanga2000@gmail.com>
2022-04-24 23:20:17 +0300

    new laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+Route::get('/',[UsersController::class,'index']);
